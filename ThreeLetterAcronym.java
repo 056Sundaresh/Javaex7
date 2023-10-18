ea7b851ca240e926c34cc1c61ac2dd586124ae9d
056Sundaresh <121413949+056Sundaresh@users.noreply.github.com>
2023-10-18 22:27:12 +0530

    Update ThreeLetterAcronym.java

--- ThreeLetterAcronym.java
+++ ThreeLetterAcronym.java
@@ -4,22 +4,16 @@ public class ThreeLetterAcronym{
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             System.out.println("Enter three words to generate a three-letter acronym:");
-           
             String userInput = input.nextLine();
-            String[] words = userInput.split(" "); // Split the input into words
-           
-            // Initialize an empty acronym
+            String[] words = userInput.split(" "); 
             StringBuilder acronym = new StringBuilder();
-           
-            // Build the acronym from the first letter of each word (up to three words)
             for (int i = 0; i < Math.min(3, words.length); i++) {
-                if (!words[i].isEmpty()) { // Ignore empty words
+                if (!words[i].isEmpty()) { 
                     acronym.append(words[i].charAt(0));
                 }
             }
            
-            String acronymResult = acronym.toString().toUpperCase(); // Convert to uppercase
-           
+            String acronymResult = acronym.toString().toUpperCase(); 
             System.out.println("Three-Letter Acronym: " + acronymResult);
         }
     }
